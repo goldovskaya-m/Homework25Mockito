bf742c0dafeb674bcf086434d07234de2e2a2aaa
goldovskaya-m <ariya-aaa@yandex.ru>
2024-10-26 18:01:18 +0300

    Initial commit

---
+++ src/main/java/sky/pro/Homework25Mokito/Homework25MokitoApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.Homework25Mokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework25MokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework25MokitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/Homework25Mokito/Homework25MokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.Homework25Mokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework25MokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
